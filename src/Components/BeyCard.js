f70d6a97271429319bfea50a9aa08262bea26938
github username <github email address>
2019-03-13 16:09:10 -0400

    completed fetching project

--- src/Components/BeyCard.js
+++ src/Components/BeyCard.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
 class BeyCard extends React.Component {
+
+// state = {
+//   delete: this.props.delete
+// }
+//  handleClick = () => {
+//    this.setState({delete:true})
+//  }
+
   render() {
+// console.log(this.state.delete)
+  // if(this.state.delete != "false"){
+  //   return <div></div>
+  // }else{
     return (
       <div>
-        <h3>{/*Bey name goes here*/}</h3>
-        {/*image tag goes here*/}
+        <h3>{this.props.bey.name}</h3>
+        <img onClick={()=> this.props.onClick(this.props.bey)} src={this.props.bey.img} alt='blank'/>
       </div>
     );
+
+  // }
   }
 }
 
--- src/Containers/BeyContainer.js
+++ src/Containers/BeyContainer.js
@@ -1,13 +1,33 @@
 import React from "react";
+import BeyCard from '../Components/BeyCard'
+
 
 class BeyContainer extends React.Component {
-  state = {};
+
+  state = {
+    beyImages:[]
+  };
+
+  componentDidMount(){
+
+    fetch('http://localhost:3001/beys').then(resp=>resp.json())
+    .then(beyImages=>this.setState({beyImages}))
+  }
+
+  handleClick=(beyObj)=>{
+    // console.log(e.target.parentElement.id)
+    let newState = this.state.beyImages.filter(ele=>ele.id != beyObj.id)
+    this.setState({beyImages:newState})
+  }
 
   render() {
+
+    let beyCards = this.state.beyImages.map(image=><BeyCard key={image.id} bey={image} onClick={this.handleClick}/>)
+
     return (
       <div className="index">
         <h1>Index</h1>
-        {/*render list*/}
+        <div>{beyCards}</div>
       </div>
     );
   }
